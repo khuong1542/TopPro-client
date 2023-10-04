ef97436c26ad5d82776aa4ab2e33e52d3e03ce24
khuong1542 <khuong1542@gmail.com>
2023-10-04 23:41:41 +0700

    'update'

--- src/app/modules/layouts/footer/footer.component.ts
+++ src/app/modules/layouts/footer/footer.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  // D:\PROJECT\TopPro\client\src\
+  baseUrl: any = document.location.origin;
+  imagePath: any = '/TopPro/dist/assets/img/footer/icon-1.png';
+  constructor(
+    private router: Router
+    ){
+      console.log(router.url)
+    }
 
+  ngOnInit() {
+    // console.log(this.router.parseUrl('/123'));
+    console.log(window.location);
+    console.log(document.location);
+  }
 }
