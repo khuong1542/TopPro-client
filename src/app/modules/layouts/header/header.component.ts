09e0edf11fe80d5c054cb890fed10b9d717d502f
khuong1542 <khuong1542@gmail.com>
2023-10-19 21:16:34 +0700

    sticky header

--- src/app/modules/layouts/header/header.component.ts
+++ src/app/modules/layouts/header/header.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  styleScroll: any;
+  shadow_sm: any;
+
+  ngOnInit(){
+    window.addEventListener('scroll', this.scroll, true);
+  }
+  scroll = (event: any): void => {
+    const n = event.srcElement.scrollingElement.scrollTop;
+    if(n > 300){
+      this.shadow_sm = 'shadow-sm';
+      this.styleScroll = 'top: 0;';
+    }else{
+      this.shadow_sm = '';
+      this.styleScroll = 'top: -100px;';
+    }
+  }
+
 }
