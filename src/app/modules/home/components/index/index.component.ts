416cad960d8d555a95cde2ae4a0a14583f4cb72f
khuong1542 <khuong1542@gmail.com>
2023-11-03 23:32:41 +0700

    'update'

--- src/app/modules/home/components/index/index.component.ts
+++ src/app/modules/home/components/index/index.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+declare var particlesJS: any;
+
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent {
-
+  public ngOnInit(): void {
+    particlesJS.load('particles-js', '../../../../../assets/js/particles.json', null);
+  }
 }
