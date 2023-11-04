ab22a9c6a9d570e79258a75b4ffa500873662146
khuongnv <khuongnv@efy.com.vn>
2023-11-04 16:59:36 +0700

    'update'

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,15 +1,18 @@
+import { MaterialModule } from './../core/load.material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { LoginRoutingModule } from './login-routing.module';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MaterialModule
   ],
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RegisterComponent],
 })
 export class LoginModule { }
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  eye_show: any = 'visibility';
+  typePass: any = 'password';
 
+  showHidePass(){
+    if(this.typePass == 'password'){
+      this.typePass = 'text';
+      this.eye_show = 'visibility_off';
+    }else{
+      this.typePass = 'password';
+      this.eye_show = 'visibility';
+    }
+  }
 }
